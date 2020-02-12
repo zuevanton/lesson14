1d3f9f0b6886cbba5f72b5b495f3ba8cb7ca5547
Anton Zuev <wnfgnqpw@gmail.com>
2020-02-12 21:49:44 +0300

    выполнил усложненное

--- script.js
+++ script.js
@@ -26,7 +26,35 @@ DomElement.prototype.createElement = function(){
                           font-size: ${this.fontSize}px;
                           `;
   element.textContent = 'check';
+  return element;
 };
 
-const domElem = new DomElement('#asd', 200, 300, 'red', 25);
-domElem.createElement();
+
+document.addEventListener('DOMContentLoaded', function(){
+  const domElem = new DomElement('#asd', 100, 100, 'red', 25);
+  let el = domElem.createElement();
+  el.style.position = 'absolute';
+  el.style.left = '50%';
+  el.style.top = '50%';
+  let x = 0, y = 0;
+  document.addEventListener('keydown', (e) =>{
+    let up = 'ArrowUp',
+        left = 'ArrowLeft',
+        down = 'ArrowDown',
+        right = 'ArrowRight';
+    if(e.key === up){
+      y -= 10;
+    }
+    if(e.key === down){
+      y += 10;
+    }
+    if(e.key === left){
+      x -= 10;
+    }
+    if(e.key === right){
+      x += 10;
+    }
+    el.style.transform = `translate(${x}px, ${y}px)`;
+  });
+});
+
